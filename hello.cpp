684bb699de46f3cf89d196426544c4160d4c846d
lama <david3951445@gmail.com>
2021-12-21 18:41:52 +0800

    v2

--- hello.cpp
+++ hello.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 using namespace std;
-
-union data{
-    int a[2];
-    char c[8];
-};
-
-int main(int argc, char const *argv[])
-{
-    union data d;
-
-    d.a[0] = 3406;
-	d.a[1] = 3404;
-
-    cout << d.c[0] << endl;
-    cout << d.c[1] << endl;
-    cout << d.c[2] << endl;
-
+int main() {
+    printf("hello");
     return 0;
 }
